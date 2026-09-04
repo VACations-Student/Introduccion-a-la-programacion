0935325d3c9ad0122e7ca2842df73830e205ffe6
vacations-student <nik.vilches@gmial.com>
2026-09-03 21:11:24 -0300

    hay que empezar guia 4

---
+++ Guia_4/Ejercicio1.hs
@@ -0,0 +1,6 @@
+-- Ejercicio 1. Implementar la funcion fibonacci:: Integer ->Integer que devuelve el i-esimo numero de Fibonacci.
+-- Recordar que la secuencia de Fibonacci se define como:
+
+--          {0                          si n = 0
+-- fib(n) = {1                          si n = 1
+--          {fib(n − 1) + fib(n − 2)    en otro caso
